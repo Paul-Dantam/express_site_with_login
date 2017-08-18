878afed7ea1a129c43b7ea98b12856632a5e7337
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-08-18 09:03:50 -0500

    boilerplate mostly done

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const logger = require("morgan");
+const mustacheExpress = require("mustache-express");
+const path = require("path");
+const users = require("./data");
+const checkAuth = require("./middlewares/checkAuth");
+const sessionConfig = require("./sessionConfig");
+const app = expresss();
+
+const port = process.env.PORT || 8000;
+
+app.engine("mustache", mustacheExpress());
+app.use("views", "./views");
+app.use("view engine", "mustache");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session(sessionConfig));
+
+app.get("/", (req, res) => {
+  let reqUsername = req.body.username;
+  let reqPassword = req.body.password;
+
+  let foundUser = users.find(user => user.username === reqUsername);
+  if (!foundUser) {
+    return res.render("login", { errors: ["Please Log in"] });
+  }
+
+  if (foundUser.password === reqPassword) {
+    delete foundUser.password;
+    req.session.user = foundUser;
+    res.redirect("/");
+  } else {
+    return res.render("login", { errors: ["Password does not match."] });
+  }
+
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
